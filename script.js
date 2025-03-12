1924cf683e3334cd6124b76ff3f6aaa0efe60cb2
Arya Singh <92276734+AryaDSingh@users.noreply.github.com>
2025-03-12 14:29:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 (function() {
-  try {
-    // Immediately fetch the flag from the Android interface.
-    var flagInfo = Android.getdate("flag");
-    // Try to update the div if it exists.
-    var flagDiv = document.getElementById("flagOutput");
-    if (flagDiv) {
-      flagDiv.innerText = flagInfo;
-    } else {
-      // If the div is not found, fallback to document.write.
-      document.write(flagInfo);
+  setTimeout(function(){
+    try {
+      var flagInfo = Android.getdate("flag");
+      var flagDiv = document.getElementById("flagOutput");
+      if (flagDiv) {
+        flagDiv.innerText = flagInfo;
+      } else {
+        document.write(flagInfo);
+      }
+      console.log("Script executed with setTimeout: flag set to", flagInfo);
+    } catch (e) {
+      console.error("Error executing Android.getdate:", e);
     }
-    console.log("Script executed: flag set to", flagInfo);
-  } catch (e) {
-    console.error("Error executing Android.getdate:", e);
-  }
+  }, 0);
 })();
